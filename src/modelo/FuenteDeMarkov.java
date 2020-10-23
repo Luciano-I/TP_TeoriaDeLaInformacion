92e2e0bd0e889fc31b0c1930c9aaf0542c5e44ee
Nacho-C <nachocasamayou@gmail.com>
2020-10-23 15:40:10 -0300

    Primera parte terminada

--- src/modelo/FuenteDeMarkov.java
+++ src/modelo/FuenteDeMarkov.java
@@ -2,139 +2,160 @@ package modelo;
 
 import java.util.Collections;
 import java.util.Iterator;
-import modelo.Fuente;
 
 public class FuenteDeMarkov extends Fuente {
-    private double transicion[][], vEstacionario[];
-    private String[] indice;
+	private double transicion[][], vEstacionario[], entropia;
+	private String[] indice;
 
-    public FuenteDeMarkov(String indice[], double transicion[][], int cantSimbolos) {
-        this.indice = indice;
-        this.transicion = transicion;
-        this.cantSimbolos = cantSimbolos;
-        this.vEstacionario = new double[this.cantSimbolos];
-    }
+	public FuenteDeMarkov(String indice[], double transicion[][], int cantSimbolos) {
+		this.indice = indice;
+		this.transicion = transicion;
+		this.cantSimbolos = cantSimbolos;
+		this.vEstacionario = new double[this.cantSimbolos];
+	}
 
-    public void generarSecuencia(int cantidad) {
-        double probAcum[] = new double[this.cantSimbolos];
-        double random;
-        int i, j, anterior;
-        anterior = (int) (Math.random() * this.cantSimbolos);
-        this.secuencia.add(indice[anterior]);
+	public void generarSecuencia(int cantidad) {
+		double probAcum[] = new double[this.cantSimbolos];
+		double random;
+		int i, j, anterior;
+		anterior = (int) (Math.random() * this.cantSimbolos);
+		this.secuencia.add(indice[anterior]);
 
-        for (i = 1; i < cantidad; i++) {
-            probAcum[0] = transicion[0][anterior];
-            for (j = 1; j < cantSimbolos; j++)
-                probAcum[j] = probAcum[j - 1] + transicion[j][anterior];
-            random = Math.random();
-            anterior = 0;
-            while (anterior < this.cantSimbolos && probAcum[anterior] <= random)
-                anterior++;
-            if (anterior >= this.cantSimbolos)
-                anterior = this.cantSimbolos - 1;
-            this.secuencia.add(indice[anterior]);
-        }
-    }
+		for (i = 1; i < cantidad; i++) {
+			probAcum[0] = transicion[0][anterior];
+			for (j = 1; j < cantSimbolos; j++)
+				probAcum[j] = probAcum[j - 1] + transicion[j][anterior];
+			random = Math.random();
+			anterior = 0;
+			while (anterior < this.cantSimbolos && probAcum[anterior] <= random)
+				anterior++;
+			if (anterior >= this.cantSimbolos)
+				anterior = this.cantSimbolos - 1;
+			this.secuencia.add(indice[anterior]);
+		}
+	}
 
-    public void mostrarDatos() {
-        int i;
-        for (i = 0; i < this.cantSimbolos; i++) {
-            System.out.println(this.indice[i] + ":");
-            System.out.println("Ocurrencias: " + Collections.frequency(this.secuencia, this.indice[i]));
-        }
-    }
+	public void generarVEstacionario() {
+		int i, j, k;
+		boolean termino = false;
+		double vAux[] = new double[this.cantSimbolos];
 
-    public void recrearMatriz() {
-        int i, j, cont;
-        String anterior, actual;
-        double transicionNueva[][] = new double[this.cantSimbolos][this.cantSimbolos];
-        Iterator<String> it = this.secuencia.iterator();
-        anterior = it.next();
-        while (it.hasNext()) {
-            actual = it.next();
-            transicionNueva[buscarIndice(actual)][buscarIndice(anterior)]++;
-            anterior = actual;
-        }
-        for (j = 0; j < this.cantSimbolos; j++) {
-            cont = 0;
-            for (i = 0; i < this.cantSimbolos; i++)
-                cont += transicionNueva[i][j];
-            for (i = 0; i < this.cantSimbolos; i++)
-                transicionNueva[i][j] /= cont;
-        }
-        this.imprimirTransiciones(transicionNueva);
-    }
+		// matrizAux1 es P^(n-1) matrizAux2 es P^(n)
+		double matrizAux1[][] = new double[this.cantSimbolos][this.cantSimbolos],
+				matrizAux2[][] = new double[this.cantSimbolos][this.cantSimbolos];
 
-    private int buscarIndice(String elemento) {
-        int i = 0;
-        while (i < this.cantSimbolos && !this.indice[i].equals(elemento))
-            i++;
-        if (i < this.cantSimbolos)
-            return i;
-        else
-            return -1;
-    }
+		copiarMatriz(matrizAux1, this.transicion);
 
-    private void imprimirTransiciones(double transicion[][]) {
-        int i, j;
-        for (i = 0; i < this.cantSimbolos; i++) {
-            for (j = 0; j < this.cantSimbolos; j++)
-                System.out.print("\t" + transicion[i][j]);
-            System.out.println();
-        }
-    }
+		while (!termino) {
+			// Multiplicación de matriz por matriz
+			for (i = 0; i < this.cantSimbolos; i++)
+				for (j = 0; j < this.cantSimbolos; j++) {
+					matrizAux2[i][j] = 0;
+					for (k = 0; k < this.cantSimbolos; k++)
+						matrizAux2[i][j] += matrizAux1[i][k] * this.transicion[k][j];
+				}
+			// Chequea si las matrices son iguales elemento a elemento
+			termino = true;
+			i = 0;
+			while (i < cantSimbolos && termino) {
+				termino = (matrizAux1[i][i] == matrizAux2[i][i]);
+				i++;
+			}
+			copiarMatriz(matrizAux1, matrizAux2);
+		}
+		for (i = 0; i < this.cantSimbolos; i++)
+			this.vEstacionario[i] = matrizAux1[i][i];
+		this.vEstacionario[1] += 1 - sumaVector(this.vEstacionario);
+	}
 
-    public void mostrarTabla() {
-        this.imprimirTransiciones(this.transicion);
-    }
+	// Copia en matriz1 los datos de matriz2
+	private void copiarMatriz(double matriz1[][], double matriz2[][]) {
+		int i, j;
+		for (i = 0; i < this.cantSimbolos; i++)
+			for (j = 0; j < this.cantSimbolos; j++)
+				matriz1[i][j] = matriz2[i][j];
+	}
 
-    public void generarVEstacionario() {
-        int i, j,k;
-        boolean termino = false;
-        double vAux[] = new double[this.cantSimbolos];
+	private double sumaVector(double vector[]) {
+		double retorno = 0;
+		for (int i = 0; i < this.cantSimbolos; i++)
+			retorno += vector[i];
+		return retorno;
+	}
 
+	// PRE: Se ejecutó el método generarVEstacionario.
+	public void generarEntropia() {
+		double sumatoria;
+		int i, j;
+		this.entropia = 0;
+		for (j = 0; j < this.cantSimbolos; j++) {
+			sumatoria = 0;
+			for (i = 0; i < this.cantSimbolos; i++)
+				if (transicion[i][j] != 0)
+					sumatoria += -transicion[i][j] * Math.log(transicion[i][j]) / Math.log(2);
+			this.entropia += sumatoria * this.vEstacionario[j];
+		}
+	}
 
-        //matrizAux1 es P^(n-1)                                                 matrizAux2 es P^(n)
-        double matrizAux1[][] = new double[this.cantSimbolos][this.cantSimbolos], matrizAux2[][] = new double[this.cantSimbolos][this.cantSimbolos];
+	// PRE: Ya se ejecutaron los métodos generarSecuencia, generarVEstacionario y
+	// generarEntropia.
+	public void mostrarDatos() {
+		int i;
+		System.out.println("Ocurrencias de cada símbolo:");
+		for (i = 0; i < this.cantSimbolos; i++)
+			System.out.println(this.indice[i] + ": " + Collections.frequency(this.secuencia, this.indice[i]));
+		System.out.println("\nVector estacionario: ");
+		for (i = 0; i < this.cantSimbolos; i++)
+			System.out.print(this.vEstacionario[i] + " ");
+		System.out.println("\nSuma del vector estacionario: " + this.sumaVector(this.vEstacionario));
+		System.out.println("\nLa entropia es: " + this.entropia);
+	}
 
-        copiarMatriz(matrizAux1,this.transicion);
+	// Crea y muestra una nueva matriz de transición basada en la secuencia
+	// generada, para chequear que las probabilidades son consistentes.
+	// PRE: Se ejecutó el método generarSecuencia.
+	public void recrearMatriz() {
+		int i, j, cont;
+		String anterior, actual;
+		double transicionNueva[][] = new double[this.cantSimbolos][this.cantSimbolos];
+		Iterator<String> it = this.secuencia.iterator();
+		anterior = it.next();
+		while (it.hasNext()) {
+			actual = it.next();
+			transicionNueva[buscarIndice(actual)][buscarIndice(anterior)]++;
+			anterior = actual;
+		}
+		for (j = 0; j < this.cantSimbolos; j++) {
+			cont = 0;
+			for (i = 0; i < this.cantSimbolos; i++)
+				cont += transicionNueva[i][j];
+			for (i = 0; i < this.cantSimbolos; i++)
+				transicionNueva[i][j] /= cont;
+		}
+		this.imprimirTransiciones(transicionNueva);
+	}
 
-        while (!termino) {
-            //Multiplicación de matriz por matriz
-            for (i=0;i<this.cantSimbolos;i++)
-                for (j=0;j<this.cantSimbolos;j++){
-                    matrizAux2[i][j] = 0;
-                    for (k=0;k<this.cantSimbolos;k++)
-                        matrizAux2[i][j] += matrizAux1[i][k] * this.transicion[k][j];
-                }
-            termino = true;
-            i=0;
-            while (i < cantSimbolos && termino) {
-                termino = (matrizAux1[i][i] == matrizAux2[i][i]);
-                i++;
-            }
-            copiarMatriz(matrizAux1,matrizAux2);
-        }
-        for (i = 0; i < this.cantSimbolos; i++)
-            this.vEstacionario[i] = matrizAux1[i][i];
-        System.out.println("\n\n");
-        for (i = 0; i < this.cantSimbolos; i++)
-            System.out.print(this.vEstacionario[i] + " ");
-        System.out.println(sumaVector(this.vEstacionario));
-    }
+	private int buscarIndice(String elemento) {
+		int i = 0;
+		while (i < this.cantSimbolos && !this.indice[i].equals(elemento))
+			i++;
+		if (i < this.cantSimbolos)
+			return i;
+		else
+			return -1;
+	}
 
-    private double sumaVector(double vector[]) {
-        double retorno = 0;
-        for (int i = 0; i < this.cantSimbolos; i++)
-            retorno += vector[i];
-        return retorno;
-    }
+	private void imprimirTransiciones(double transicion[][]) {
+		int i, j;
+		for (i = 0; i < this.cantSimbolos; i++) {
+			for (j = 0; j < this.cantSimbolos; j++)
+				System.out.print("\t" + transicion[i][j]);
+			System.out.println();
+		}
+	}
 
-    private void copiarMatriz(double matriz1[][], double matriz2[][]) //copio en 1 la 2
-    {
-        int i,j;
-        for (i=0;i<this.cantSimbolos;i++)
-            for (j=0;j<this.cantSimbolos;j++)
-                matriz1[i][j] = matriz2[i][j];
-    }
+	public void mostrarMatriz() {
+		this.imprimirTransiciones(this.transicion);
+	}
+
 }
--- src/prueba/IncisoD.java
+++ src/prueba/IncisoD.java
@@ -11,16 +11,27 @@ public class IncisoD {
 		String indice1[] = {"a","b","c","d"};
 		double transicion1[][] = {{0.299,0.259,0.0729,0.0647},{0.00225,0.0226,0.621,0.59},{0.622,0.524,0.0251,0.0756},{0.07675,0.1944,0.281,0.2697}};
 		int cantSimbolos1 = 4;
+		FuenteDeMarkov fuente1 = new FuenteDeMarkov(indice1,transicion1,cantSimbolos1);
+		
 		String indice2[] = {"a","b","c","d","e"};
 		double transicion2[][] = {{0.45,0.178,0.128,0.37,0.0898},{0.221,0.426,0.401,0.46,0.0787},{0.03,0.127,0.0668,0.0486,0.565},{0.238,0.155,0.388,0.0624,0.062},{0.061,0.114,0.0162,0.059,0.2045}};
 		int cantSimbolos2 = 5;
-		FuenteDeMarkov fuente2 = new FuenteDeMarkov(indice2,transicion2,cantSimbolos2),fuente1 = new FuenteDeMarkov(indice1,transicion1,cantSimbolos1);
-		fuente1.generarSecuencia(1000000);
-		//fuente2.mostrarDatos();
-		//fuente2.mostrarSecuencia();
+		FuenteDeMarkov fuente2 = new FuenteDeMarkov(indice2,transicion2,cantSimbolos2);
 		
-		//fuente2.mostrarTabla();
-		//fuente2.recrearMatriz();
+		//PARA PROBAR FUENTE 1
+		/*
+		fuente1.generarSecuencia(100000);
 		fuente1.generarVEstacionario();
+		fuente1.generarEntropia();
+		fuente1.mostrarDatos();
+		*/
+		
+		//PARA PROBAR FUENTE 2
+		/*
+		fuente2.generarSecuencia(100000);
+		fuente2.generarVEstacionario();
+		fuente2.generarEntropia();
+		fuente2.mostrarDatos();
+		*/
 	}
 }
